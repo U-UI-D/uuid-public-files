81fa200377ddda327c5950c0ebc5d3994aa7cee2
1445654576@qq.com <1445654576@qq.com>
2020-08-22 16:06:11 +0800

    测试云效

--- deploy/uuid-config/uuid-config-deploy.sh
+++ deploy/uuid-config/uuid-config-deploy.sh
@@ -6,7 +6,7 @@ docker-compose down
 
 rm -f docker-compose.yml
 
-wget https://gitee.com/U-UI-D/uuid-public-files/raw/master/uuid-config/docker-compose.yml
+wget https://gitee.com/U-UI-D/uuid-public-files/raw/master/deploy/uuid-config/docker-compose.yml
 
 docker rmi registry.cn-shenzhen.aliyuncs.com/uuid/uuid-config:1.0.0
 
