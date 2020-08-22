4a631ecf177f0c43965514c0025da2238215d7eb
1445654576@qq.com <1445654576@qq.com>
2020-08-22 12:57:37 +0800

    添加文件

---
+++ deploy/uuid-config/uuid-config-deploy.sh
@@ -0,0 +1,13 @@
+mkdir -p /root/project/uuid/uuid-config
+
+cd /root/project/uuid/uuid-config
+
+docker-compose down
+
+rm -f docker-compose.yml
+
+wget https://gitee.com/U-UI-D/uuid-public-files/raw/master/uuid-config/docker-compose.yml
+
+docker rmi registry.cn-shenzhen.aliyuncs.com/uuid/uuid-config:1.0.0
+
+docker-compose up -d
