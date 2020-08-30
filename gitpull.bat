8ec6e88a3ff4e1dc6ca45506192a6130629acb27
1445654576@qq.com <1445654576@qq.com>
2020-08-31 07:11:06 +0800

    添加git拉取的脚本文件

---
+++ gitpull.bat
@@ -0,0 +1,47 @@
+cd ../uuid-common
+git pull
+
+cd ../uuid-config
+git pull
+
+cd ../uuid-config-repo
+git pull
+
+cd ../uuid-dependencies
+git pull
+
+cd ../uuid-dependencies-common-provider
+git pull
+
+cd ../uuid-dependencies-spring-cloud-component
+git pull
+
+cd ../uuid-eureka
+git pull
+
+cd ../uuid-gateway
+git pull
+
+cd ../uuid-package
+git pull
+
+cd ../uuid-public-files
+git pull
+
+cd ../uuid-service-common
+git pull
+
+cd ../uuid-service-sso
+git pull
+
+cd ../uuid-service-user
+git pull
+
+cd ../uuid-service-work
+git pull
+
+cd ../uuid-zipkin
+git pull
+
+cd ..
+echo "========================== PULL FINISH =========================="
