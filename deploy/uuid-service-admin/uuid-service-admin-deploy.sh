b51322e859cb822ced9ddec52062a6414739cda1
1445654576@qq.com <1445654576@qq.com>
2020-11-12 11:48:43 +0800

    更新配置

---
+++ deploy/uuid-service-admin/uuid-service-admin-deploy.sh
@@ -0,0 +1,13 @@
+mkdir -p /root/project/uuid/uuid-service-admin
+
+cd /root/project/uuid/uuid-service-admin
+
+docker-compose down
+
+rm -f docker-compose.yml
+
+wget https://gitee.com/U-UI-D/uuid-public-files/raw/master/deploy/uuid-service-admin/docker-compose.yml
+
+docker rmi registry.cn-shenzhen.aliyuncs.com/uuid/uuid-service-admin:1.0.0
+
+docker-compose up -d
