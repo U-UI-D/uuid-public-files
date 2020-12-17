eaa1cb0eb4810c2fa843558ccb951eb1e40298cd
1445654576@qq.com <1445654576@qq.com>
2020-12-17 22:07:09 +0800

    限制CPU内存

--- deploy/uuid-config/uuid-config-deploy.sh
+++ deploy/uuid-config/uuid-config-deploy.sh
@@ -10,4 +10,4 @@ wget https://gitee.com/U-UI-D/uuid-public-files/raw/master/deploy/uuid-config/do
 
 docker rmi registry.cn-shenzhen.aliyuncs.com/uuid/uuid-config:1.0.0
 
-docker-compose up -d
+docker-compose --compatibility up -d
